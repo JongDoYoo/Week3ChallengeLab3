ebbdbaa33d494da28abd0821c0ce696826adb8c6
JongD <JongD@JONGDO>
2024-05-24 14:02:28 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Week3ChallengeLab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 2, 7, 11, 15 };
+
+            int target = 9;
+
+
+        }
+        static int NumTarget(int num, int target) 
+        {
+            int left = 0;
+            int right = num.length -1;
+        
+            while (left < right)
+            {
+                int sum = nums[left] + [right];
+            }
+        }
+        
+    }
+
+}
+
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].3. Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
